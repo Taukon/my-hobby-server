763e66aa6475f4c3a47a919ee83786730d1e861e
Taukon <83957880+Taukon@users.noreply.github.com>
2024-03-16 18:02:30 +0900

    fix

--- frontend/src/app/browser/tesseract/tickTC2.tsx
+++ frontend/src/app/browser/tesseract/tickTC2.tsx
@@ -448,7 +448,7 @@ const LogicConf: React.FC<{
           ref={overRef}
           type="number"
           step={0.001}
-          min={0.001}
+          min={-0.001}
           max={1}
           defaultValue={tcm.conf.over}
         />
